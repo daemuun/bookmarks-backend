7ad520aef1bb181d47251f90496a13e25e74410f
Dudoladov Dmitry <dudmitry.iv@gmail.com>
2025-11-04 20:04:37 +0300

    refactor: create User methods for hashing

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt"
+
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
@@ -6,5 +8,15 @@ const userSchema = new Schema({
     hashedPasswd: { type: String, required: true },
     userName: { type: String, default: "User", trim: true, minLength: 2, maxLength: 50 },
 }, { timestamps: true });
+
+
+userSchema.methods.verifyPasswd = async function(passwd) {
+    return await bcrypt.compare(passwd, this.hashedPasswd);
+}   
+
+userSchema.statics.hashPasswd = async (passwd) => {
+    const saltRounds = 12;
+    return await bcrypt.hash(passwd, saltRounds);
+}
 
 export const User = mongoose.model("User", userSchema);
